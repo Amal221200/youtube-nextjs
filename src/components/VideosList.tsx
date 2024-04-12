6451a841654fd53d0a5ac915122433ab73645c88
Amal221200 <amalmurikkoli2000@gmail.com>
2024-04-12 15:57:42 +0530

    m

--- src/components/VideosList.tsx
+++ src/components/VideosList.tsx
@@ -5,7 +5,7 @@ import VideoItem from "./VideoItem"
 const VideosList = ({ videos }: { videos: VideoItemType[] }) => {
 
   return (
-    <main className="grid md:grid-cols-3 sm:grid-cols-2 gap-3 md:justify-between justify-center h-[90dvh] overflow-y-scroll w-full">
+    <main className="grid md:grid-cols-3 sm:grid-cols-2 gap-3 gap-y-4 md:justify-between justify-center h-[90dvh] overflow-y-scroll w-full">
       {
         videos.map((video) => (
           <VideoItem data={video} key={video.id.videoId} />
