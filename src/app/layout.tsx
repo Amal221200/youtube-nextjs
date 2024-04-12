4ce6acf1591968825d031cd881be0410a69bda72
Amal221200 <amalmurikkoli2000@gmail.com>
2024-04-12 16:53:33 +0530

    dark mode mobile sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={cn(inter.className, "overflow-hidden")}>
         <AuthProvider>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem
             disableTransitionOnChange>
             <ProgressBarProvider>
               {children}
--- src/components/MobileSidebar.tsx
+++ src/components/MobileSidebar.tsx
@@ -8,8 +8,8 @@ const MobileSidebar = () => {
     const { isOpen, onClose } = useSidebar()
 
     return (
-        <aside className={cn('absolute top-0 left-0 w-full h-full bg-white/40 z-50 transition duration-500 transform', isOpen ? "translate-x-0" : "-translate-x-full")}>
-            <div className="relative h-full max-w-[300px] bg-white border">
+        <aside className={cn('absolute top-0 left-0 w-full h-full bg-white/40 dark:bg-black/40 z-50 transition duration-500 transform', isOpen ? "translate-x-0" : "-translate-x-full")}>
+            <div className="relative h-full max-w-[300px] bg-white dark:bg-black border dark:border-gray-800">
                 <button type="button" onClick={onClose} className="absolute top-3 right-3 hover:opacity-70 transition">
                     <CircleX />
                 </button>
